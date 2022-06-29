9607fa0369fdc8741d3aa3ad24c08e0c1f37b31d
Halmai McRae <98130939+halmai-mcrae@users.noreply.github.com>
2022-06-29 17:03:40 +1200

    Create HomeScreenHeader component

---
+++ frontend/src/components/HomeScreenHeader.jsx
@@ -0,0 +1,23 @@
+import './HomeScreenHeader.css'
+
+const HomeScreenHeader = () => {
+  return (
+    <>
+    <div className="background">
+  <div className="homescreen-header">
+    <h2 className="homescreen-header__title">
+      Can't decide? We don't blame you.
+      </h2>
+    <h3>
+      Try one of each, or indulge with a favourite.
+      </h3>
+    <div className="homescreen-header__products">
+
+
+    </div>
+  </div>
+  </div>
+  </>
+)}
+
+export default HomeScreenHeader
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,8 +1,21 @@
+import HomeScreenHeader from '../components/HomeScreenHeader'
 import './HomeScreen.css'
 
 const HomeScreen = () => {
   return (
-  <div className="homescreen">Home screen</div>
+    <>
+  <div className="homescreen">
+    <HomeScreenHeader />
+    <h2 className="homescreen__title">
+      Products
+      </h2>
+    <div className="homescreen__products">
+      <div></div>
+
+
+    </div>
+  </div>
+  </>
 )}
 
 export default HomeScreen
