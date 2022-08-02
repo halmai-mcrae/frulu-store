2e7b6ec1856df9b553a01f3d9a0a0d4a8f039c42
Halmai McRae <98130939+halmai-mcrae@users.noreply.github.com>
2022-08-03 10:22:14 +1200

    Add CartItem.jsx

---
+++ frontend/src/components/CartItem.jsx
@@ -0,0 +1,34 @@
+import './CartItem.css';
+
+import { Link } from 'react-router-dom';
+
+const CartItem = () => {
+  return (
+    <div className="cartitem">
+      <div className="cartitem-image">
+        <img src="https://images.unsplash.com/photo-1595475207225-428b62bda831?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80" alt=""/>
+      </div>
+      <Link to={`/product/${111}`} className="cartitem__name">
+        <p>Product 1</p>
+        </Link>
+
+        <p className="cartitem__price">$1.99</p>
+
+        <select className="cartitem__select">
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          </select>
+
+        <button className="cartitem__remove-button">
+        <i className="fas fa-trash"></i>
+        </button>
+      
+
+  
+    </div>
+  );
+}
+
+export default CartItem;
--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -1,10 +1,26 @@
 import './CartScreen.css'
 
+// Components
+import CartItem from '../components/CartItem'
+
 const CartScreen = () => {
   return (
-  <div className="cart-screen">
-    Cart Screen
-  </div>
+  <div className="cartscreen">
+    <div className="cartscreen-left">
+    <CartItem />
+    </div>
+    <div className="cartscreen-right">
+      <div className="cartscreen-info">
+        <p>(0) items</p>
+        <p>$1.99</p>
+        </div>
+        <div>
+        <button>Checkout</button>
+        </div>
+    </div>
+    </div>
+
+
 )}
 
 export default CartScreen
