0ac240c3a8e52a57c63632533ab91919d46c151b
Halmai McRae <98130939+halmai-mcrae@users.noreply.github.com>
2022-08-26 16:33:14 +1200

    Items in cart persist after refresh

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -21,8 +21,19 @@ const reducer = combineReducers({
 
 const middleware = [thunk]
 
+const cartItemsInLocalStorage = localStorage.getItem('cart')
+  ? JSON.parse(localStorage.getItem('cart'))
+  : []
+
+const INITIAL_STATE = {
+  cart: {
+    cartItems: cartItemsInLocalStorage,
+  },
+}
+
 const store = configureStore(
   { reducer },
+  INITIAL_STATE,
   composeWithDevTools(applyMiddleware(...middleware))
 )
 
